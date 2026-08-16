61244533f992e6ee3535ca710aeccdbea870c38f
Meaadwithy <meaad.alqthmi145@gmail.com>
2026-08-16 07:10:56 +0300

    Create Model class for character customization
    This class defines various attributes for a character model, including skin, eyes, hair, and outfits with their corresponding images and names.

---
+++ 2. Model.java
@@ -0,0 +1,29 @@
+public class Model {
+    public int currentSkin = 0;
+    public int currentEyes = 0;
+    public int currentHair = 0;
+    public int currentHairColor = 0;
+    public int currentOutfit = 0;
+    public int currentOutfitColor = 0;
+
+
+    public final String[] SKINS = {
+            "images/skin1.png", "images/skin2.png", "images/skin3.png", "images/skin4.png"
+    };
+    public final String[] SKIN_NAMES = {"بشرة 1 ", "بشرة 2 ", "بشرة 3 ", "بشرة 4 "};
+
+    public final String[] EYES_COLORS = {"images/color1.png","images/color2.png","images/color3.png","images/color4.png","images/color5.png"};
+
+    public final String[] HAIR = {"images/hair1.1.png","images/hair2.1.png"};
+    public final String[] HAIR1_COLORS = {"images/hair1.1.png","images/hair1.2.png","images/hair1.3.png","images/hair1.4.png","images/hair1.5.png","images/hair1.6.png"};
+    public final String[] HAIR2_COLORS = {"images/hair2.1.png","images/hair2.2.png","images/hair2.3.png","images/hair2.4.png","images/hair2.5.png","images/hair2.6.png"};
+    public final String[] HAIR_NAMES = {"شعر 1 ","شعر 2 "};
+
+    public final String[] OUTFIT = { "images/outfit1.1.png","images/outfit2.1.png" ,"images/outfit3.1.png" ,"images/outfit4.1.png" };
+    public final String[] OUTFIT_1 = {"images/outfit1.1.png","images/outfit1.2.png","images/outfit1.3.png","images/outfit1.4.png"};
+    public final String[] OUTFIT_2 = {"images/outfit2.1.png","images/outfit2.2.png","images/outfit2.3.png","images/outfit2.4.png"};
+    public final String[] OUTFIT_3 = {"images/outfit3.1.png","images/outfit3.2.png","images/outfit3.3.png","images/outfit3.4.png"};
+    public final String[] OUTFIT_4 = {"images/outfit4.1.png","images/outfit4.2.png","images/outfit4.3.png","images/outfit4.4.png"};
+
+    public final String[] OUTFIT_NAMES = {"لبس 1 ","لبس 2 ","لبس 3 ","لبس 4 "};
+}
